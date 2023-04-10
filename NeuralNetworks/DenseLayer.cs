54bf8e2894383ad5b1fe889630404eb6fe8bea58
Ivan Iochev <I.IOCHEV@rimsoft.bg>
2023-04-11 00:13:37 +0300

    neural

---
+++ NeuralNetworks/DenseLayer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Media;
+using MathNet.Numerics.LinearAlgebra;
+
+namespace NeuralNetworks;
+
+public class DenseLayer
+{
+    public int InputSize { get; }
+    public int Units { get; }
+    public Func<Matrix<double>, Matrix<double>> ActivationFunction { get; }
+    public Matrix<double> Weight { get; }
+    public Vector<double> Bias { get; }
+
+
+    public DenseLayer(int inputSize, int units, Func<Matrix<double>, Matrix<double>> activationFunction)
+    {
+        InputSize = inputSize;
+        Units = units;
+        ActivationFunction = activationFunction;
+
+        Weight = Matrix<double>.Build.Dense(inputSize, units);
+        Bias = Vector<double>.Build.Dense(units);
+    }
+
+    public Matrix<double> ForwardPropagate(Matrix<double> input)
+    {
+        var biasMatrix = Matrix<double>.Build.DenseOfColumns(Enumerable.Repeat(Bias, Units));
+        return ActivationFunction(input.Multiply(Weight).Add(biasMatrix));
+    }
+
+}
---
+++ NeuralNetworks/NeuralNetworkModel.cs
@@ -0,0 +1,52 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetworks
+{
+    internal class NeuralNetworkModel
+    {
+        public DenseLayer[] Layers { get; }
+
+        public NeuralNetworkModel(params DenseLayer[] layers)
+        {
+            Layers = layers;
+        }
+
+        public double ComputeCost(Matrix<double> input, Vector<double> target)
+        {
+            var predictions = Predict(input);
+
+            var predictionsExp = predictions.PointwiseExp();
+            var divisors = predictionsExp.RowSums();
+
+            var top = Vector<double>.Build.DenseOfEnumerable(target.Select((x, i) => predictions[i, (int)target[i] - 1]));
+            double cost = top.PointwiseDivide(divisors).PointwiseLog().Sum() / -input.RowCount;
+
+            Debug.WriteLine("Cost: " + cost);
+
+            return cost;
+        }
+
+        private Matrix<double> Predict(Matrix<double> input)
+        {
+            var current = input;
+            foreach (var layer in Layers)
+            {
+                current = layer.ForwardPropagate(current);
+            }
+
+            var predictions = current;
+            return predictions;
+        }
+
+        public Vector<double> BackPropagate(Vector<double> target)
+        {
+
+        }
+    }
+}
