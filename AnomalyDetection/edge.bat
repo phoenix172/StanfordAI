f901c57a1f48fee80457a9fc4ebbef943a5eb235
Ivan Iochev <I.IOCHEV@rimsoft.bg>
2023-12-07 14:38:55 +0200

    runs and draws test cases

---
+++ AnomalyDetection/edge.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" -remote-debugging-port=9222 --user-data-dir="C:\Users\i.iochev\AppData\Local\Temp\blazor-edge-debug" --no-first-run http:\\localhost:5263
